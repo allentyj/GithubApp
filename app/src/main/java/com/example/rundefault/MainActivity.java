9a47a4ca8c82ddbf0d5584467dd1eac587478eae
Stuppo <allentyj@gmail.com>
2021-01-31 23:15:19 -0500

    Second commit - made changes to the MAinActivity.java

---
+++ app/src/main/java/com/example/rundefault/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.rundefault;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Hello");
+    }
+
+    //TODO: put in more code
+
+}
